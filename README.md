eb06cf92d4cba51bd137ee83a6408274ed44c596
ali.cruz@me.com <ali.cruz@me.com>
2024-10-11 08:11:40 -0600

    Usando capacitor para mandarlo a android

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # ionic-1
+
+## Entorno
+- Angular CLI: 18.2.8 
+- Node v18.20.4
+- Ionic 7.2.0
+
+
+## Compilar para Web
+- ionic build
+
+## Crear APK
+- npm install @capacitor/android
+- npx cap add android
+- npx cap open android
+
+
+
--- src/app/lista-productos/lista-productos.page.ts
+++ src/app/lista-productos/lista-productos.page.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 import { RouterLink } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-lista-productos',
   templateUrl: './lista-productos.page.html',
   styleUrls: ['./lista-productos.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, RouterLink]
+  imports: [CommonModule, FormsModule, RouterLink, IonicModule]
 })
 export class ListaProductosPage implements OnInit {
 
